f3a9f42641194f359a327b111e54b831bdd6a083
Cristhian Ledezma <cristhianenrique23@gmail.com>
2020-04-23 13:23:50 +0200

    Prueba final

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.1.94:2020', {'forceNew':true});
+var socket = io.connect('http://localhost:2020', {'forceNew':true});
 
 socket.on ('messages', function(data){
     console.log(data);
@@ -9,11 +9,26 @@ function render (data){
     var html = data.map(function(message, index){
         return (`
         <div class="message">
-        <strong> ${mesagge.nickname} </strong> dice:
+        <strong> ${message.nickname} </strong> dice:
         <p> ${message.text} <p/>
         </div>
         `);
 
-    }); . join ('');
-    document.getElementById('messages').innerHTML() = html;
+    }) . join ('');
+    var div_msgs = document.getElementById('messages')
+     div_msgs.innerHTML = html;
+     div_msgs.scrollTop = div_msgs.scrollHeight;
+
+}
+
+function addMessage(e){
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+
+    };
+
+    document.getElementById('nickname').style.display= 'none';
+    socket.emit('add-message', message);
+    return false;
 }
--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ var messages =[{
 
 io.on('connection', function(socket){
     console.log("El cliente con IP: " +socket.handshake.address+ " se ha conectado");
+
     socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+    });
 });
 
 server.listen(2020,function (){
